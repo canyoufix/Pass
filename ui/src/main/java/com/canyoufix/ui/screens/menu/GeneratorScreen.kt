e4e810f7a0b0ad99ec0b1c1a03c74262ab583e3c
raspberry tea <sekiro.fpl@gmail.com>
2025-03-24 02:17:02 +0700

    Update GeneratorScreen

--- ui/src/main/java/com/canyoufix/ui/screens/menu/GeneratorScreen.kt
+++ ui/src/main/java/com/canyoufix/ui/screens/menu/GeneratorScreen.kt
@@ -1,11 +1,8 @@
 package com.canyoufix.ui.screens.menu
 
-import android.content.ClipboardManager
-import android.content.Context
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,37 +10,35 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.canyoufix.ui.R
-import com.canyoufix.ui.utils.generatePassword
+import com.canyoufix.ui.utils.ClipboardUtils
+import com.canyoufix.ui.utils.PasswordUtils
 
 @Composable
 fun GeneratorScreen() {
     var password by remember { mutableStateOf("") }
-    var length by remember { mutableStateOf(12) }
+    var length by remember { mutableIntStateOf(25) }
     var useLowerCase by remember { mutableStateOf(true) }
     var useUpperCase by remember { mutableStateOf(true) }
     var useDigits by remember { mutableStateOf(true) }
@@ -51,25 +46,14 @@ fun GeneratorScreen() {
 
     // Получаем Context для доступа к ClipboardManager
     val context = LocalContext.current
-    val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
 
     // Логика для копирования пароля
     val copyToClipboard: () -> Unit = {
-        val clip = android.content.ClipData.newPlainText("password", password)
-        clipboardManager.setPrimaryClip(clip)
+        ClipboardUtils.copyToClipboard(context, "password", password)  // Используем утилиту
     }
 
     // Проверяем, что хотя бы один свитч включен
     val isAnySwitchOn = useLowerCase || useUpperCase || useDigits || useSymbols
-
-    // Логика принудительного включения ползунка с маленькими буквами
-    val handleLowerCaseSwitch: (Boolean) -> Unit = { newValue ->
-        if (!useUpperCase && !useDigits && !useSymbols) {
-            useLowerCase = true // Принудительно включаем, если все выключены
-        } else {
-            useLowerCase = newValue
-        }
-    }
 
     // Scroll для прокрутки контента
     val scrollState = rememberScrollState()
@@ -82,85 +66,110 @@ fun GeneratorScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         // Итоговый сгенерированный пароль
-        Row(
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .border(1.dp, Color.Gray)
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.Gray.copy(alpha = 0.1f)) // Серый фон с прозрачностью
+                .padding(16.dp)
         ) {
             Text(
                 text = password,
                 style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold), // Уменьшенный шрифт
-                modifier = Modifier.weight(1f)
+                modifier = Modifier.align(Alignment.CenterStart) // Выравнивание текста по левому краю
             )
         }
+        Spacer(modifier = Modifier.height(16.dp))
 
-        // Кнопка копирования вне рамки с паролем
-        IconButton(
-            onClick = copyToClipboard,
-            modifier = Modifier
-                .size(40.dp) // Размер кнопки
-                .padding(8.dp)
-                .background(MaterialTheme.colorScheme.primary, shape = CircleShape) // Кнопка с фоном
-                .align(Alignment.End) // Выравнивание кнопки справа
+        // Row для размещения кнопок горизонтально
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp), // Отступ между кнопками
+            verticalAlignment = Alignment.CenterVertically // Выравнивание по центру по вертикали
         ) {
-            val icon: Painter = painterResource(id = R.drawable.ic_copy) // Замените на свою иконку
-            Image(painter = icon, contentDescription = "Copy")
+            // Кнопка для генерации пароля, делаем неактивной, если все свитчи выключены
+            Button(
+                onClick = {
+                    password = PasswordUtils.generatePassword(length, useLowerCase, useUpperCase, useDigits, useSymbols)
+                },
+                enabled = isAnySwitchOn, // Кнопка неактивна, если все свитчи выключены
+                modifier = Modifier.weight(1f) // Делает кнопку растягиваемой
+            ) {
+                Text("Сгенерировать")
+            }
+
+            // Кнопка для копирования пароля
+            Button(
+                onClick = copyToClipboard,
+                enabled = password.isNotEmpty(),
+                modifier = Modifier.weight(1f) // Делает кнопку растягиваемой
+            ) {
+                Text("Скопировать")
+            }
         }
-
         Spacer(modifier = Modifier.height(16.dp))
 
-        // Кнопка для генерации пароля
-        Button(onClick = {
-            password = generatePassword(length, useLowerCase, useUpperCase, useDigits, useSymbols)
-        }) {
-            Text("Сгенерировать пароль")
+        // Подложка с ползунком для изменения длины пароля
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.Gray.copy(alpha = 0.1f)) // Серый фон
+                .padding(16.dp)
+        ) {
+            Column {
+                Text("Длина пароля: $length")
+                Slider(
+                    value = length.toFloat(),
+                    onValueChange = { length = it.toInt() }, // Блокировка ползунка при выключении всех свитчей
+                    valueRange = 8f..128f, // Теперь от 8 до 128
+                    modifier = Modifier.padding(bottom = 5.dp) // Отступ снизу
+                )
+            }
         }
+        Spacer(modifier = Modifier.height(24.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.Gray.copy(alpha = 0.1f)) // Серый фон
+                .padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth() // Используем Column для вертикального расположения
+            ) {
+                SwitchWithLabel(
+                    label = "Строчные буквы",
+                    checked = useLowerCase,
+                    onCheckedChange = { useLowerCase = it }
+                )
 
-        // Ползунок для изменения длины пароля
-        Text("Длина пароля: $length")
-        Slider(
-            value = length.toFloat(),
-            onValueChange = { if (isAnySwitchOn) length = it.toInt() }, // Блокировка ползунка при выключении всех свитчей
-            valueRange = 8f..128f, // Теперь от 8 до 128
-            modifier = Modifier.padding(bottom = 16.dp) // Отступ снизу
-        )
+                Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
+                SwitchWithLabel(
+                    label = "Заглавные буквы",
+                    checked = useUpperCase,
+                    onCheckedChange = { useUpperCase = it }
+                )
 
-        // Свитчи для выбора типа символов
-        SwitchWithLabel(
-            label = "Строчные буквы",
-            checked = useLowerCase,
-            onCheckedChange = { handleLowerCaseSwitch(it) }
-        )
+                Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(8.dp))
+                SwitchWithLabel(
+                    label = "Цифры",
+                    checked = useDigits,
+                    onCheckedChange = { useDigits = it }
+                )
 
-        SwitchWithLabel(
-            label = "Заглавные буквы",
-            checked = useUpperCase,
-            onCheckedChange = { if (isAnySwitchOn) useUpperCase = it }
-        )
+                Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(8.dp))
-
-        SwitchWithLabel(
-            label = "Цифры",
-            checked = useDigits,
-            onCheckedChange = { if (isAnySwitchOn) useDigits = it }
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        SwitchWithLabel(
-            label = "Спецсимволы",
-            checked = useSymbols,
-            onCheckedChange = { if (isAnySwitchOn) useSymbols = it }
-        )
+                SwitchWithLabel(
+                    label = "Спецсимволы",
+                    checked = useSymbols,
+                    onCheckedChange = { useSymbols = it }
+                )
+            }
+        }
     }
 }
 
--- ui/src/main/java/com/canyoufix/ui/utils/PasswordUtils.kt
+++ ui/src/main/java/com/canyoufix/ui/utils/PasswordUtils.kt
@@ -2,28 +2,30 @@ package com.canyoufix.ui.utils
 
 import java.security.SecureRandom
 
-fun generatePassword(
-    length: Int,
-    useLowerCase: Boolean,
-    useUpperCase: Boolean,
-    useDigits: Boolean,
-    useSymbols: Boolean
-): String {
-    val lowerCaseChars = "abcdefghijklmnopqrstuvwxyz"
-    val upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    val digits = "0123456789"
-    val symbols = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/\\"
+object PasswordUtils {
+    fun generatePassword(
+        length: Int,
+        useLowerCase: Boolean,
+        useUpperCase: Boolean,
+        useDigits: Boolean,
+        useSymbols: Boolean
+    ): String {
+        val lowerCaseChars = "abcdefghijklmnopqrstuvwxyz"
+        val upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        val digits = "0123456789"
+        val symbols = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/\\"
 
-    var characters = ""
-    if (useLowerCase) characters += lowerCaseChars
-    if (useUpperCase) characters += upperCaseChars
-    if (useDigits) characters += digits
-    if (useSymbols) characters += symbols
+        var characters = ""
+        if (useLowerCase) characters += lowerCaseChars
+        if (useUpperCase) characters += upperCaseChars
+        if (useDigits) characters += digits
+        if (useSymbols) characters += symbols
 
-    if (characters.isEmpty()) throw IllegalArgumentException("At least one character type must be selected")
+        if (characters.isEmpty()) throw IllegalArgumentException("At least one character type must be selected")
 
-    val random = SecureRandom()
-    return (1..length)
-        .map { characters[random.nextInt(characters.length)] }
-        .joinToString("")
+        val random = SecureRandom()
+        return (1..length)
+            .map { characters[random.nextInt(characters.length)] }
+            .joinToString("")
+    }
 }
